9157b818fa39ece91af9b6dfd1cf35ddea19e1f2
JoseMarioCarranza <imt_josecarranza@outlook.com>
2024-12-23 04:56:10 -0700

    Cambios minimos para deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configura la URL base del backend
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Cambia esta URL si tu backend está en otro lugar
+    baseURL: process.env.REACT_APP_API_URL, // Cambia esta URL si tu backend está en otro lugar
 });
 
 // Interceptor para incluir el token JWT en cada solicitud
